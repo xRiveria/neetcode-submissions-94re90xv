84e7845acc4d478af8eef5f692a9df965b710510
Ryan Tan Wen Ter <50167924+xRiveria@users.noreply.github.com>
2026-04-02 01:22:22 +0800

    Add: network-delay-time - submission-7

---
+++ Data Structures & Algorithms/network-delay-time/submission-7.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+
+        // Time.
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> poop;
+        
+        std::set<int> visitedNodes;
+        std::unordered_map<int, std::vector<std::pair<int, int>>> edges; // adj list.
+        for (const auto& time : times)
+        {
+            edges[time[0]].push_back({ time[1], time[2] });
+        }
+
+        poop.push({ 0, k });
+        
+        int t = 0;
+
+        while (!poop.empty())
+        {
+            if (visitedNodes.size() == n)
+            {
+                break;
+            }
+
+            std::pair<int, int> f = poop.top();
+            poop.pop();
+
+            if (visitedNodes.count(f.second)) continue; // Possible if there are items in the queue unvisited yet and we push more.
+
+            visitedNodes.insert(f.second);
+            t = f.first;
+
+            for (auto& nei : edges[f.second])
+            {
+                if (!visitedNodes.count(nei.first))
+                {
+                    poop.push({ f.first + nei.second, nei.first });
+                }
+            }
+
+            
+        }
+
+        if (visitedNodes.size() == n) return t;
+        return -1; 
+    }
+};
