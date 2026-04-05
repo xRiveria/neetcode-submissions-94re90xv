dcdf990310ed0483f9bec2328efb2c87ecf64c4a
Ryan Tan Wen Ter <50167924+xRiveria@users.noreply.github.com>
2026-04-05 23:50:26 +0800

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            if s[i].isalnum() == False:
+                i += 1
+            elif s[j].isalnum() == False:
+                j -= 1
+            elif s[i].lower() != s[j].lower():
+                return False
+            else:
+                i += 1
+                j -= 1
+
+        return True
